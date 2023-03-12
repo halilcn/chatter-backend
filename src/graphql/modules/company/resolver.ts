954ee96da42a4beaea6d03b28ef0cc1b54a16037
halilcn <iletisimhalilcan@gmail.com>
2023-03-12 23:13:21 +0300

    update error catcher

--- src/graphql/modules/company/resolver.ts
+++ src/graphql/modules/company/resolver.ts
@@ -1,25 +1,19 @@
-import { GraphQLError } from 'graphql';
 import { v4 as uuidv4 } from 'uuid';
 
 import Company from '../../../models/company';
 import { ICompanyData, ICompanyInputData } from '../../../types';
 import errorCatcher from '../../../utils/errorCatcher';
-import { BadRequest } from '../../../utils/errors';
 
 export default {
-  createCompany: async ({ input }: { input: ICompanyInputData }): Promise<ICompanyData> => {
+  createCompany: errorCatcher(async ({ input }: { input: ICompanyInputData }) => {
     const { name } = input;
     const company = (await Company.create({ name, key: uuidv4() })).toJSON();
 
     return company;
-  },
-  companies: async () => {
-    // TODO: response null problem
-    await errorCatcher(async () => {
-      throw new BadRequest();
-      const companies = await Company.find({});
+  }),
+  companies: errorCatcher(async () => {
+    const companies = await Company.find({});
 
-      return companies;
-    });
-  }
+    return companies;
+  })
 };
--- src/utils/errorCatcher.ts
+++ src/utils/errorCatcher.ts
@@ -1,11 +1,11 @@
 import { GraphQLError } from 'graphql';
 import { StatusCodes } from 'http-status-codes';
 
-export default async (func: any) => {
-  try {
-    const result = await func();
-    return result;
-  } catch (err: any) {
-    throw new GraphQLError(err.message || 'BAD_REQUEST', null, null, null, null, null, { code: err.status || StatusCodes.BAD_REQUEST });
-  }
-};
+export default (processFunction: (...args: any) => Promise<GraphQLError | object>) =>
+  async (...args: any) => {
+    try {
+      return await processFunction(...args);
+    } catch (err: any) {
+      throw new GraphQLError(err.message || 'BAD_REQUEST', null, null, null, null, null, { code: err.status || StatusCodes.BAD_REQUEST });
+    }
+  };
